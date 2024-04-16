4c8db5a3fcacf8c73513bae5001742f150a46f46
Sarah <srf3@hood.edu>
2024-04-16 17:00:32 -0400

    Got rid of unnecessary line on dashboard page

--- alep_frontend/src/components/Dashboard.js
+++ alep_frontend/src/components/Dashboard.js
@@ -7,7 +7,6 @@ function Dashboard() {
     <div className="Dashboard">
 
       <h2>Welcome to Your Dashboard</h2>
-      <p>Here you can apply for a new loan, view your loan applications, and more.</p>
       <Link to="/apply" className='button'>Apply for a New Loan</Link>
       <span className='button-space'></span>
       <Link to="/loan-applications" className='button'>View Your Loan Applications</Link>
